ab84148e7dee8a136e3841195eb3855d01d155bc
Junhui Huang <95193970+NightFall28@users.noreply.github.com>
2024-09-07 19:07:32 -0400

    authentication
    sign up, sign in, clerk middleware, logo added

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,33 @@
-const SignInPage = () => {
-    return ( 
-        <div>
-            Sign In Page
+import { SignIn, ClerkLoading, ClerkLoaded } from '@clerk/nextjs'
+import { Loader2 } from 'lucide-react';
+import Image from 'next/image';
+
+export default function Page() {
+  return (
+    <div className='min-h-screen grid grid-cols-1 lg:grid-cols-2'>
+        <div className='h-full lg:flex flex-col items-center justify-center px-4'>
+            <div className='text-center space-y-4 pt-6'>
+                <h1 className='text-4xl font-extrabold'> 
+                    Welcome Back!
+                </h1>
+                <p className='text-gray-600'>
+                    Sign in to your account to get back to your dashboard!
+                </p>
+            </div>
+
+            <div className='flex items-center justify-center mt-6'>
+                <ClerkLoaded>
+                    <SignIn path='/sign-in'/>
+                </ClerkLoaded>
+                <ClerkLoading>
+                    <Loader2 className='animate-spin text-muted-foreground' />
+                </ClerkLoading>
+            </div>
         </div>
-     );
+
+        <div className='h-full bg-blue-600 hidden lg:flex items-center justify-center'>
+            <Image src="/logoipsum.svg" height={100} width={100} alt='Logo'/>
+        </div>
+    </div>
+);
 }
- 
-export default SignInPage;
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,33 @@
-const SignUpPage = () => {
-    return ( 
-        <div>
-            Sign Up Page
-        </div>
-     );
-}
+import { SignUp, ClerkLoading, ClerkLoaded } from '@clerk/nextjs'
+import { Loader2 } from 'lucide-react';
+import Image from 'next/image';
 
-export default SignUpPage;
+export default function Page() {
+  return (
+    <div className='min-h-screen grid grid-cols-1 lg:grid-cols-2'>
+        <div className='h-full lg:flex flex-col items-center justify-center px-4'>
+            <div className='text-center space-y-4 pt-6'>
+                <h1 className='text-4xl font-extrabold'> 
+                    Welcome!
+                </h1>
+                <p className='text-gray-600'>
+                    Sign up for an account to get back to your dashboard!
+                </p>
+            </div>
+
+            <div className='flex items-center justify-center mt-6'>
+                <ClerkLoaded>
+                    <SignUp path='/sign-up'/>
+                </ClerkLoaded>
+                <ClerkLoading>
+                    <Loader2 className='animate-spin text-muted-foreground' />
+                </ClerkLoading>
+            </div>
+        </div>
+
+        <div className='h-full bg-blue-600 hidden lg:flex items-center justify-center'>
+            <Image src="/logoipsum.svg" height={100} width={100} alt='Logo'/>
+        </div>
+    </div>
+);
+}
