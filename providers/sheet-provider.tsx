03020759f0e3e98046dbd60d83770c5437d97640
Junhui Huang <95193970+NightFall28@users.noreply.github.com>
2024-09-19 22:46:45 -0400

    data-table component
    shadcn-ui data-table component added, fixed globals.css

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -2,7 +2,6 @@
 
 import { useMountedState } from "react-use";
 import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
-import { useEffect, useState } from "react";
 
 export const SheetProvider = () => {
     const isMounted = useMountedState()
