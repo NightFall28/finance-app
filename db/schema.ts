c67d6e0a71bae5ece9cc6ce6744d438960d81941
Junhui Huang <95193970+NightFall28@users.noreply.github.com>
2024-09-20 22:09:52 -0400

    Transactions API & Schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,7 @@
-import { pgTable, text } from "drizzle-orm/pg-core";
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
+import { relations } from 'drizzle-orm';
+import { z } from "zod";
 
 export const accounts = pgTable("accounts", {
     id: text("id").primaryKey(),
@@ -18,3 +20,45 @@ export const categories = pgTable("categories", {
 })
 
 export const insertCategorySchema = createInsertSchema(categories)
+
+export const transactions = pgTable("transactions", {
+    id: text("id").primaryKey(),
+    amount: integer("amount").notNull(),
+    payee: text("payee").notNull(),
+    notes: text("notes"),
+    date: timestamp("date", {mode: 'date'}).notNull(),
+
+    // foreign keys
+    accountId: text("account_id").references(() => accounts.id, {
+        // when an account is deleted, delete all transactions associated with it
+        onDelete: 'cascade',
+    }).notNull(),
+
+    categoryId: text("category_id").references(() => categories.id, {
+        // when a category is deleted, set all transactions of that category uncategorized
+        onDelete: 'set null',
+    }),
+})
+
+export const insertTransactionSchema = createInsertSchema(transactions, {
+    date: z.coerce.date(),
+})
+
+export const accountsRelations = relations(accounts, ({many}) => ({
+    transactions: many(transactions)
+}))
+
+export const categoriesRelations = relations(categories, ({many}) => ({
+    transactions: many(transactions)
+}))
+
+export const transactionsRelations = relations(transactions, ({one}) => ({
+    account: one(accounts, {
+        fields: [transactions.accountId],
+        references: [accounts.id],
+    }),
+    category: one(categories, {
+        fields: [transactions.categoryId],
+        references: [categories.id],
+    }),
+}))
